ba8270440420fc9a02a9622105555ee2a1d5fc3a
Paulo Diovani <paulo@diovani.com>
2020-03-09 20:36:02 -0300

    Add /attendees/awarded endpoint

--- examples/ruby/gift_giver/app/controllers/attendees_controller.rb
+++ examples/ruby/gift_giver/app/controllers/attendees_controller.rb
@@ -3,4 +3,9 @@ class AttendeesController < ApplicationController
     attendees = Attendee.all
     render json: attendees
   end
+
+  def awarded
+    attendees = Attendee.awarded
+    render json: attendees
+  end
 end
--- examples/ruby/gift_giver/config/routes.rb
+++ examples/ruby/gift_giver/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get :attendees, to: 'attendees#index'
+  get :'attendees/awarded', to: 'attendees#awarded'
   post :'attendees/draw', to: 'draw#index'
   post :refresh, to: 'refresh#index'
 end
