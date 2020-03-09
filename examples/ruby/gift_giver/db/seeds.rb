28350e96e35a4fb65748a4c23ff2361ae98a7d3c
Paulo Diovani <paulo@diovani.com>
2020-03-09 20:22:38 -0300

    We don't need seeds anymore

--- examples/ruby/gift_giver/db/seeds.rb
+++
@@ -1,8 +0,0 @@
-20.times do |number| 
-    Attendee.create!(
-        name: "Fulano #{number}", 
-        email: "fulano#{number}@example.com", 
-        languages: "javascript,ruby",
-        awarded: false
-    )
-end
