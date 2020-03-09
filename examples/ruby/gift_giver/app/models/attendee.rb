25baa122e8506eefc243f4bf45d9aceefe0d5073
Paulo Diovani <paulo@diovani.com>
2020-03-09 20:27:29 -0300

    Add awarded scope
    - fix indentation

--- examples/ruby/gift_giver/app/models/attendee.rb
+++ examples/ruby/gift_giver/app/models/attendee.rb
@@ -1,5 +1,7 @@
 class Attendee < ApplicationRecord
-    validates :vendor_user_id, presence: true
-    validates :vendor_user_id, uniqueness: true
-    validates :awarded, inclusion: { in: [ true, false ] }
+  validates :vendor_user_id, presence: true
+  validates :vendor_user_id, uniqueness: true
+  validates :awarded, inclusion: { in: [ true, false ] }
+
+  scope :awarded, -> { where(awarded: true) }
 end
