34043eeba5fcc6a39495728c183b5841ec2b4902
Paulo Diovani <paulo@diovani.com>
2020-03-10 12:58:05 -0300

    Address pr comments

--- examples/ruby/gift_giver/app/models/attendee.rb
+++ examples/ruby/gift_giver/app/models/attendee.rb
@@ -1,6 +1,5 @@
 class Attendee < ApplicationRecord
-  validates :vendor_user_id, presence: true
-  validates :vendor_user_id, uniqueness: true
+  validates :vendor_user_id, presence: true, uniqueness: true
   validates :awarded, inclusion: { in: [ true, false ] }
 
   scope :awarded, -> { where(awarded: true) }
--- examples/ruby/gift_giver/config/routes.rb
+++ examples/ruby/gift_giver/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get :attendees, to: 'attendees#index'
-  get :'attendees/awarded', to: 'attendees#awarded'
-  post :'attendees/draw', to: 'draw#index'
+  resources :attendees, only: [:index] do
+    collection do
+      get :awarded
+      post :draw, to: 'draw#index'
+    end
+  end
+
   post :refresh, to: 'refresh#index'
 end
