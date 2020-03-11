bdc12fd83ac46a426b9cba0895091b4690b27d5a
Paulo Diovani <paulo@diovani.com>
2020-03-11 12:58:09 -0300

    Init java spring project

---
+++ examples/java-spring/giftgiver/src/main/java/io/github/valetechtalks/giftgiver/GiftgiverApplication.java
@@ -0,0 +1,13 @@
+package io.github.valetechtalks.giftgiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftgiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftgiverApplication.class, args);
+	}
+
+}
