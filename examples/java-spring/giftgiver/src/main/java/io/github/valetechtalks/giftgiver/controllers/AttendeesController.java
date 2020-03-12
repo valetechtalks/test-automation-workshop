ee8079dc47424a818a894878be3965be80930167
Paulo Diovani <paulo@diovani.com>
2020-03-12 15:24:47 -0300

    List attendees

--- examples/java-spring/giftgiver/src/main/java/io/github/valetechtalks/giftgiver/controllers/AttendeesController.java
+++ examples/java-spring/giftgiver/src/main/java/io/github/valetechtalks/giftgiver/controllers/AttendeesController.java
@@ -1,16 +1,37 @@
 package io.github.valetechtalks.giftgiver.controllers;
 
-import java.util.concurrent.atomic.AtomicLong;
 import io.github.valetechtalks.giftgiver.entities.Attendee;
+import io.github.valetechtalks.giftgiver.services.DatabaseSession;
+import io.github.valetechtalks.giftgiver.services.MeetupConsumer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class AttendeesController {
-    private final AtomicLong counter = new AtomicLong();
+    private DatabaseSession db;
+    private MeetupConsumer meetup;
+
+    public AttendeesController() {
+        this.db = new DatabaseSession();
+        this.meetup = new MeetupConsumer();
+    }
 
     @GetMapping("/attendees")
-    public Attendee Index() {
-        return new Attendee(1, (long) 999, "Paulo", "", false);
+    public List<Attendee> Index() {
+        List<Attendee> attendees = new ArrayList<Attendee>();
+
+        try {
+            this.db.open();
+            attendees = this.db.getAll(Attendee.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally{
+            this.db.close();
+        }
+
+        return attendees;
     }
 }
