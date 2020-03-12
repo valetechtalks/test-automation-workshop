90a5a4d8aa651c63301e85c3ddbd591f87b9eec7
Paulo Diovani <paulo@diovani.com>
2020-03-12 11:13:43 -0300

    Rename app

--- examples/java-spring/giftgiver/src/main/java/io/github/valetechtalks/giftgiver/GiftGiverApplication.java
+++ examples/java-spring/giftgiver/src/main/java/io/github/valetechtalks/giftgiver/GiftGiverApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GiftgiverApplication {
+public class GiftGiverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GiftgiverApplication.class, args);
+		SpringApplication.run(GiftGiverApplication.class, args);
 	}
 
 }
